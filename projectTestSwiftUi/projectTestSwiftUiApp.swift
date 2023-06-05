e86f43bede740733c72fb3547b250d0dfd6e838f
ehsan2793 <ehsan2793@gmail.com>
2023-06-05 16:42:11 -0700

    Initial Commit

---
+++ projectTestSwiftUi/projectTestSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectTestSwiftUiApp.swift
+//  projectTestSwiftUi
+//
+//  Created by Ehsan Rahimi on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct projectTestSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
