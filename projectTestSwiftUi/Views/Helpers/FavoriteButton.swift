2d15d291c5d1b5f59f2a7551067b39e05aa53f59
ehsan2793 <ehsan2793@gmail.com>
2023-06-06 01:40:31 -0700

    favorite button added

---
+++ projectTestSwiftUi/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  projectTestSwiftUi
+//
+//  Created by Ehsan Rahimi on 6/6/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }, label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+
+        })
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
