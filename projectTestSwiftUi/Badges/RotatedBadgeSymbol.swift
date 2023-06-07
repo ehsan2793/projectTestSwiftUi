06049619db0353920356875a88bafbb6c30c1bcc
ehsan2793 <ehsan2793@gmail.com>
2023-06-07 00:14:00 -0700

    shape added

---
+++ projectTestSwiftUi/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,26 @@
+//
+//  RotatedBadgeSymbol.swift
+//  projectTestSwiftUi
+//
+//  Created by Ehsan Rahimi on 6/6/23.
+//
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
