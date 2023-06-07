86307aacb8e1a33c5e7942b1823f97ac628f3171
ehsan2793 <ehsan2793@gmail.com>
2023-06-07 00:55:35 -0700

    grapth + animation added

--- projectTestSwiftUi/Model/Hike.swift
+++ projectTestSwiftUi/Model/Hike.swift
@@ -12,17 +12,18 @@ struct Hike: Codable, Hashable, Identifiable {
     var name: String
     var distance: Double
     var difficulty: Int
-    var observations: [Observations]
+    var observations: [Observation]
 
-    static var formater = LengthFormatter()
+    static var formatter = LengthFormatter()
 
     var distanceText: String {
-        Hike.formater
+        Hike.formatter
             .string(fromValue: distance, unit: .kilometer)
     }
 
-    struct Observations: Codable, Hashable {
+    struct Observation: Codable, Hashable {
         var distanceFromStart: Double
+
         var elevation: Range<Double>
         var pace: Range<Double>
         var heartRate: Range<Double>
