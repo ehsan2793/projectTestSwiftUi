18b239fc935500cb34d056646b60c64f169578c2
ehsan2793 <ehsan2793@gmail.com>
2023-06-08 03:43:25 -0700

    add new view

---
+++ projectTestSwiftUi/Views/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  projectTestSwiftUi
+//
+//  Created by Ehsan Rahimi on 6/8/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
