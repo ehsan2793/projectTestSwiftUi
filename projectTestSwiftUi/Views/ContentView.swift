745864d5fbe8d1620ae30b58375f45e7d5869ad5
ehsan2793 <ehsan2793@gmail.com>
2023-06-08 04:25:33 -0700

    tabview added

--- projectTestSwiftUi/Views/ContentView.swift
+++ projectTestSwiftUi/Views/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
+    @State private var selection: Tab = .featured
+    enum Tab {
+          case featured
+          case list
+      }
 
     // MARK: - BODY
 
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+              CategoryHome()
+                  .tag(Tab.featured)
+
+              LandmarkList()
+                  .tag(Tab.list)
+          }
     }
 }
 
