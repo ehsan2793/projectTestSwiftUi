8afeacc77fbf2ce634386d369f1d9816945e35ad
ehsan2793 <ehsan2793@gmail.com>
2023-06-08 04:16:05 -0700

    catagories added

--- projectTestSwiftUi/Model/ModelData.swift
+++ projectTestSwiftUi/Model/ModelData.swift
@@ -5,14 +5,16 @@ final class ModelData: ObservableObject {
 
     var hikes: [Hike] = load("hikeData.json")
 
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+
     var categories: [String: [Landmark]] {
-         Dictionary(
-             grouping: landmarks,
-             by: { $0.category.rawValue }
-         )
-     }
-    
-    
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
