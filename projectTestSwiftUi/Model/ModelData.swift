022af9af19c9990d4c512def1b564ea876480a28
ehsan2793 <ehsan2793@gmail.com>
2023-06-08 04:00:40 -0700

    added vaigation view

--- projectTestSwiftUi/Model/ModelData.swift
+++ projectTestSwiftUi/Model/ModelData.swift
@@ -2,8 +2,17 @@ import Combine
 import Foundation
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
-    
+
     var hikes: [Hike] = load("hikeData.json")
+
+    var categories: [String: [Landmark]] {
+         Dictionary(
+             grouping: landmarks,
+             by: { $0.category.rawValue }
+         )
+     }
+    
+    
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
---
+++ projectTestSwiftUi/Views/CategoryRow.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryRow.swift
+//  projectTestSwiftUi
+//
+//  Created by Ehsan Rahimi on 6/8/23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
