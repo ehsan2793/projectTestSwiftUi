94e676463895991c51cf894e6dfe24c77838a56c
ehsan2793 <ehsan2793@gmail.com>
2023-06-08 21:23:13 -0700

    added user sheet

--- projectTestSwiftUi/Views/Profiles/ProfileHost.swift
+++ projectTestSwiftUi/Views/Profiles/ProfileHost.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ProfileHost: View {
     @State private var draftProfile = Profile.default
     var body: some View {
-        Text("Profile for: \(draftProfile.username)")
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+           
+        }
+        .padding()
     }
 }
 
